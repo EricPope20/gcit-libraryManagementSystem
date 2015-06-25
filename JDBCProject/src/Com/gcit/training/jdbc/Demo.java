3d9249d3d97d83687619f91a10ec615dbec9471d
ericackaah <ericackaah@192.168.0.25>
2015-06-25 18:04:41 -0400

    Initial Commit

---
+++ JDBCProject/src/Com/gcit/training/jdbc/Demo.java
@@ -0,0 +1,44 @@
+package Com.gcit.training.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		try {
+			//building the connection
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "");
+			Statement stmt = conn.createStatement();
+			String selectQuery = "select * from tbl_author";
+			ResultSet rs = stmt.executeQuery(selectQuery);
+			
+			while(rs.next()){
+				System.out.println("Author ID: " +rs.getInt("authorId"));
+				System.out.println("Author Name: " +rs.getString("authorName"));
+				System.out.println("-----------------------------");
+			}
+			
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Enter a new Author: ");
+			String authorName = scan.nextLine();
+			
+			String createQuery = "insert into tbl_author (authorName) values('" +authorName+"')";
+			
+			stmt.executeUpdate(createQuery);
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
+
+
