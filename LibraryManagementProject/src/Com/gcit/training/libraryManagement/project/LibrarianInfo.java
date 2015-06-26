1b1e7c431a39ecf5e565b0274b505cbe048df1b7
ericackaah <ericackaah@192.168.0.2>
2015-06-26 02:03:16 -0400

    Updates on Librarian class: Letting user make their library selection

--- LibraryManagementProject/src/Com/gcit/training/libraryManagement/project/LibrarianInfo.java
+++ LibraryManagementProject/src/Com/gcit/training/libraryManagement/project/LibrarianInfo.java
@@ -12,42 +12,60 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class LibrarianInfo {
-	//data fields
-	private static int userChoice;
-	//constructor
-	public LibrarianInfo(){
+	// data fields
+	private String userString = "";
+
+	// constructor
+	public LibrarianInfo() {
 		try {
-			//displaying the available branches to the user
+			// displaying the available branches to the user
 			System.out.println("Here are the available Library Branches");
 			System.out.println("---------------------------------------");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "");
-			//Statement stmt = conn.createStatement();
+			Connection conn = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/library", "root", "");
+			// Statement stmt = conn.createStatement();
 			String selectQuery = "select * from tbl_library_branch";
-			
+
 			PreparedStatement pstmt = conn.prepareStatement(selectQuery);
 			ResultSet rs = pstmt.executeQuery();
-			
-			//loop
-			while(rs.next()){
-				System.out.println("Branch Name: " +rs.getString("branchName"));
-				System.out.println("Branch Address: " +rs.getString("branchAddress"));
-				System.out.println("-----------------------------");
+
+			int count = 1;
+			// loop
+			while (rs.next()) {
+				System.out.println(count + ") " + rs.getString("branchName")
+						+ ", " + rs.getString("branchAddress"));
+				count++;
 			}
-			//making selection
+			System.out.println(count + ") Quit to previous");
+
+			// making selection
 			Scanner scan = new Scanner(System.in);
-			System.out.println("Select the Library Branch you want to update:");
-			String libBranch = scan.nextLine();
-			
-			//condition
-			
-			
+			System.out
+					.println("Select the Library Branch you want to update based on their number");
+			int libBranch = scan.nextInt();
+			scan.nextLine();
+
+			// using switch condition to take the user input
+			switch (libBranch) {
+			case 1:
+
+				System.out
+						.println("You selected the University Library,"
+								+ "do you want to:\n1) Update the details of the Library"
+								+ "\n2)Add copies of Book to the Branch\n"
+								+ "3)Quit to previous");
+				userString = scan.nextLine();
+
+				break;
+			case 2:
+
+			default:
+				break;
+			}
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 
 	}
 }
-
-
-
-
--- LibraryManagementProject/src/Com/gcit/training/libraryManagement/project/LibraryManagement.java
+++ LibraryManagementProject/src/Com/gcit/training/libraryManagement/project/LibraryManagement.java
@@ -6,100 +6,104 @@
  */
 
 package Com.gcit.training.libraryManagement.project;
+
 import java.util.Scanner;
 
 public class LibraryManagement {
-	//fields for constant recall
+	// fields for constant recall
 	private static int librarian = 1;
 	private static final int administrator = 2;
 	private static final int borrower = 3;
-	
+
 	public static void main(String[] args) {
 		firstPageReturn();
-	
-}
+
+	}
+
 	static int userStatus;
-	//the first page to display
-	private static void firstPageReturn(){
+
+	// the first page to display
+	private static void firstPageReturn() {
 		String userString = " ";
-		
+
 		System.out.println("Welcome to the GCIT Library Management System");
-		System.out.println("---------------------------------------------\n1)." + 
-							"Librarian \n2).Administrator\n3).Borrower");
+		System.out.println("---------------------------------------------\n1)."
+				+ "Librarian \n2).Administrator\n3).Borrower");
 		System.out.println("Which category of a user are you.?");
-		
+
 		// getting input
 		Scanner in = new Scanner(System.in);
 		userStatus = in.nextInt();
-		
-		//based on their status this will help determine the class to invoke
-		while(userStatus <= 0){
-			if(userStatus == 0){
-				System.out.println("Sorry, your category must be between 1 to 3");
+
+		// based on their status this will help determine the class to invoke
+		while (userStatus <= 0) {
+			if (userStatus == 0) {
+				System.out
+						.println("Sorry, your category must be between 1 to 3");
 				System.out.println("Which category of user are you.?");
 				userStatus = in.nextInt();
 			}
 		}
-		
-			//invoking the user category method
-			userCategory();
+
+		// invoking the user category method
+		userCategory();
 	}
-	
-	//the method to determine the user category
-	private static void userCategory(){
-		
+
+	// the method to determine the user category
+	private static void userCategory() {
+
 		Scanner input = new Scanner(System.in);
 		String userString = " ";
-		
-		//if user is librarian
-		if(userStatus == 1){
-			System.out.println("You choose a category of "+ librarian + 
-							          ", Librarian, do you want to continue? (y/n)");
+
+		// if user is librarian
+		if (userStatus == 1) {
+			System.out.println("You choose a category of " + librarian
+					+ ", Librarian, do you want to continue? (y/n)");
 			userString = input.nextLine();
-			//if the user decides to move on
-			while(userString.toLowerCase().equals("y")){
+			// if the user decides to move on
+			while (userString.toLowerCase().equals("y")) {
 				LibrarianInfo myLB = new LibrarianInfo();
 				break;
-				
+
 			}
-			if(userString.toLowerCase().equals("n"))
+			if (userString.toLowerCase().equals("n"))
 				firstPageReturn();
-		  //validate
-			if(!userString.toLowerCase().equals("y") || !userString.toLowerCase().equals("n")){
+			// validate
+			if (!userString.toLowerCase().equals("y")
+					|| !userString.toLowerCase().equals("n")) {
 				System.out.println("Sorry you have entered invalid response");
 				firstPageReturn();
 			}
-	}
-		//if the user is administrator
-		else if(userStatus == 2){
-			System.out.println("You choose a category of, "+ administrator + 
-				          " Administrator, do you want to continue? (y/n)");
+		}
+		// if the user is administrator
+		else if (userStatus == 2) {
+			System.out.println("You choose a category of, " + administrator
+					+ " Administrator, do you want to continue? (y/n)");
 			userString = input.nextLine();
-			//if the user decides to move on
-			while(userString.toLowerCase().equals("y")){
-			 AdministratorInfo myAD = new AdministratorInfo();
-			break;
-		
-		}   //if the input is no
-			if(userString.toLowerCase().equals("n"))
-			firstPageReturn();
-	}
-		
-		//if the user is Borrower
-				else if(userStatus == 3){
-					System.out.println("You choose a category of, "+ borrower + 
-						          " Borrower, do you want to continue? (y/n)");
-					userString = input.nextLine();
-					//if the user decides to move on
-					while(userString.toLowerCase().equals("y")){
-					 BorrowerInfo myAD = new BorrowerInfo();
-					break;
-				
-				}   //if the input is no
-					if(userString.toLowerCase().equals("n"))
-					firstPageReturn();
-			}
-	}
-	
-}
+			// if the user decides to move on
+			while (userString.toLowerCase().equals("y")) {
+				AdministratorInfo myAD = new AdministratorInfo();
+				break;
 
+			} // if the input is no
+			if (userString.toLowerCase().equals("n"))
+				firstPageReturn();
+		}
+
+		// if the user is Borrower
+		else if (userStatus == 3) {
+			System.out.println("You choose a category of, " + borrower
+					+ " Borrower, do you want to continue? (y/n)");
+			userString = input.nextLine();
+			// if the user decides to move on
+			while (userString.toLowerCase().equals("y")) {
+				BorrowerInfo myAD = new BorrowerInfo();
+				break;
+
+			} // if the input is no
+			if (userString.toLowerCase().equals("n"))
+				firstPageReturn();
+		}
+	}
+
+}
